df2daf70a541ec313cee1f31da5084c87be715cf
RobbieGoldberg <robbiebryce1998@gmail.com>
2022-05-28 13:57:02 -0700

    Update WordEvaluator.cs
    changed dictionary to a hashtable, created method for checking validity of word

--- Assets/Scripts/WordEvaluator.cs
+++ Assets/Scripts/WordEvaluator.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class WordEvaluator : MonoBehaviour
 {
-	
 	public TextAsset dictionary;
 	
-	private List<string> wordList;
+	private Hashtable wordList;
 	
     void Start()
     {
         string allWords = dictionary.text;
-		wordList = new List<string>();
-		wordList.AddRange(allWords.Split("\n"[0]));
+		wordList = new Hashtable();
+		
+		foreach (string word in allWords.Split("\n"[0]))
+		{
+			wordList.Add(word, true);
+		}
     }
+
+	public bool IsValidWord(string word)
+	{
+		return wordList.Contains(word.ToUpper());
+	}
 
     void Update()
     {
-        
+        Debug.Log(IsValidWord("test"));
     }
 }
