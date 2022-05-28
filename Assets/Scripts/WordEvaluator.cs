375c7b664996cdfbb595276e71842727fc885940
RobbieGoldberg <robbiebryce1998@gmail.com>
2022-05-28 14:03:18 -0700

    Update WordEvaluator.cs
    added hashtable for letter values

--- Assets/Scripts/WordEvaluator.cs
+++ Assets/Scripts/WordEvaluator.cs
@@ -8,6 +8,8 @@ public class WordEvaluator : MonoBehaviour
 	
 	private Hashtable wordList;
 	
+	private Hashtable letterValues;
+	
     void Start()
     {
         string allWords = dictionary.text;
@@ -17,6 +19,34 @@ public class WordEvaluator : MonoBehaviour
 		{
 			wordList.Add(word, true);
 		}
+		
+		letterValues = new Hashtable();
+		letterValues.Add('A', 1);
+		letterValues.Add('B', 3);
+		letterValues.Add('C', 3);
+		letterValues.Add('D', 2);
+		letterValues.Add('E', 1);
+		letterValues.Add('F', 4);
+		letterValues.Add('G', 2);
+		letterValues.Add('H', 4);
+		letterValues.Add('I', 1);
+		letterValues.Add('J', 8);
+		letterValues.Add('K', 5);
+		letterValues.Add('L', 1);
+		letterValues.Add('M', 3);
+		letterValues.Add('N', 1);
+		letterValues.Add('O', 1);
+		letterValues.Add('P', 3);
+		letterValues.Add('Q', 10);
+		letterValues.Add('R', 1);
+		letterValues.Add('S', 1);
+		letterValues.Add('T', 1);
+		letterValues.Add('U', 1);
+		letterValues.Add('V', 4);
+		letterValues.Add('W', 4);
+		letterValues.Add('X', 8);
+		letterValues.Add('Y', 4);
+		letterValues.Add('Z', 10);
     }
 
 	public bool IsValidWord(string word)
