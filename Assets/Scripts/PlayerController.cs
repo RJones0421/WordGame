48be10ce5d60162e3ed70b4be0f819849e72289f
Jason Chen <chenjase@umich.edu>
2022-05-30 18:17:43 -0400

    added jumping functionality to player, collision boxes added around base floor and player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,5 +20,15 @@ public class PlayerController : MonoBehaviour
         movement *= Time.deltaTime;
 
         transform.Translate(movement);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+        	// Vector3 jump_movement = new Vector3(inputX, 5f, inputY);
+        	// jump_movement *= Time.deltaTime;
+
+        	// transform.Translate(jump_movement);
+
+        	GetComponent<Rigidbody2D>().velocity = Vector2.up * 8;
+        }
     }
 }
