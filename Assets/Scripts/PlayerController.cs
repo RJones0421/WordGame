8099bead27ad397861cb3dec16e7c792b97f2241
Jason Chen <chenjase@umich.edu>
2022-05-30 22:06:36 -0400

    recommented jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
