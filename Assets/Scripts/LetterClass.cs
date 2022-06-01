db0ef92b9ccdc5307b6b756bc73d59b699877de8
RobbieGoldberg <robbiebryce1998@gmail.com>
2022-05-31 20:04:34 -0700

    created letter class and objects for every letter

---
+++ Assets/Scripts/LetterClass.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="Letter")]
+public class LetterClass : ScriptableObject
+{
+	[SerializeField]
+	private char letter;
+	
+	[SerializeField]
+	private Sprite letterSprite;
+	
+	[SerializeField]
+	private int score;
+}
