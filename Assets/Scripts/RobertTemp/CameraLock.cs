b2cb27710a068f4ad7a2d3ca5c80f843cec79808
RJones0421 <orangestreak0421@gmail.com>
2022-06-01 17:31:19 -0700

    Basic movement
    Implemented the basic movement for a prototype that I am exploring

---
+++ Assets/Scripts/RobertTemp/CameraLock.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CameraLock : MonoBehaviour
+{
+    [SerializeField]
+    Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Camera.main.transform.position = new Vector3(0, player.position.y, Camera.main.transform.position.z);
+    }
+}
