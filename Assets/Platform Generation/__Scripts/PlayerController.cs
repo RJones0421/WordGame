b980dff8d48ddf69658f650f035b3059fcebbecb
Jae Shim <jaeshim@usc.edu>
2022-06-01 17:05:42 -0700

    More consistent physics

--- Assets/Platform Generation/__Scripts/PlayerController.cs
+++ Assets/Platform Generation/__Scripts/PlayerController.cs
@@ -4,53 +4,64 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private bool jump;
-    private bool air;
+    //private bool jump;
+    //private bool air;
+    private Vector2 down;
     private Rigidbody2D rb;
-    // private MeshCollider2D mc;
-    private BoxCollider2D box;
-    private PlatformEffector2D effector;
+    //private BoxCollider2D box;
+    //private PlatformEffector2D effector;
 
     // Start is called before the first frame update
     void Start()
     {
-        jump = false;
-        air = true;
+        //jump = false;
+        //air = true;
+        down = Vector2.down * 2;
         rb = GetComponent<Rigidbody2D>();
-        box = GetComponent<BoxCollider2D>();
-        effector = GetComponent<PlatformEffector2D>();
-        // mc = GetComponent<MeshCollider>();
+        //box = GetComponent<BoxCollider2D>();
+        //effector = GetComponent<PlatformEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float inputX = Input.GetAxis("Horizontal");
+        Camera.main.transform.position = new Vector3(0.0f, Camera.main.transform.position.y, -1.0f);
+        rb.velocity = down;
 
-        Vector3 movement = new Vector3(10 * inputX, 0, 0);
+        Debug.LogFormat("VELOCITY: {0} {1}", rb.velocity.x, rb.velocity.y);
+
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        if (inputY < 0.0f)
+        {
+            inputY = 0.0f;
+        }
+
+        Vector3 movement = new Vector3(10 * inputX, 10 * inputY, 0);
         movement *= Time.deltaTime;
 
         transform.Translate(movement);
 
 
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (!jump && !air)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, 10.0f);
-            }
+        //if (Input.GetButtonDown("Jump"))
+        //{
+        //    if (!jump && !air)
+        //    {
+        //        rb.velocity = new Vector2(rb.velocity.x, 10.0f);
+        //    }
 
-            jump = true;
-            air = true;
-        }
-        else
-        {
-            jump = false;
-        }
+        //    jump = true;
+        //    air = true;
+        //}
+        //else
+        //{
+        //    jump = false;
+        //}
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        air = false;
+        //air = false;
     }
 }
