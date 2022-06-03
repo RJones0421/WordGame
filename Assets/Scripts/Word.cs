e712ee4eed764178672f4b7eff09fa9d12c458f7
td-usc <89547497+td-usc@users.noreply.github.com>
2022-06-03 10:56:23 -0700

    Connected UI to word class
    Connected UI elements so that word class can now add letters and submit. Temporarily used Enter for submit.

--- Assets/Scripts/Word.cs
+++ Assets/Scripts/Word.cs
@@ -5,12 +5,31 @@ using UnityEngine;
 public class Word : MonoBehaviour
 {
 
-    [SerializeField] WordEvaluator evaluator;
+    [SerializeField] private WordEvaluator evaluator;
+
+    [SerializeField] private Sprite defaultSprite;
+
+    [SerializeField] private List<SpriteRenderer> sprites = new List<SpriteRenderer>();
 
     private List<LetterClass> letters = new List<LetterClass>();
 
-    public void addLetter(LetterClass newLetter) {
-        letters.Add(newLetter);
+    private int currentLetterBox = 0;
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Return)) submitWord();
+    }
+
+    public bool addLetter(LetterClass newLetter) {
+        if (newLetter.Letter == '_') return false;
+
+        else {
+            letters.Add(newLetter);
+            sprites[currentLetterBox].sprite = newLetter.LetterSprite;
+            currentLetterBox++;
+        }
+
+        return true;
+
     }
 
     public int submitWord() {
@@ -25,7 +44,11 @@ public class Word : MonoBehaviour
 
         int score = evaluator.SubmitWord(word);
 
-        if (score != 0) letters.Clear();
+        if (score != 0) {
+            letters.Clear();
+            foreach (SpriteRenderer sr in sprites) sr.sprite = defaultSprite;
+            currentLetterBox = 0;
+        }
 
         return score;
 
