9fda994e63773c4ab902d2401fbc0ea16f8fe585
RJones0421 <orangestreak0421@gmail.com>
2022-06-04 14:18:33 -0700

    Updated build
    Updated build to be the direction we chose to go. Cleaned up some of the old prototype stuff

--- Assets/Scripts/Hotkeys.cs
+++ Assets/Scripts/Hotkeys.cs
@@ -9,21 +9,13 @@ public class Hotkeys : MonoBehaviour
             Restart();
         }
 
+        if (Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            SceneManager.LoadScene(0);
+        }
         if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
         {
             SceneManager.LoadScene(1);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
-        {
-            SceneManager.LoadScene(2);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
-        {
-            SceneManager.LoadScene(3);
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad4))
-        {
-            SceneManager.LoadScene(4);
         }
     }
  
