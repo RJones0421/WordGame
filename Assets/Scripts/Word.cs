78cde202c55dc2b7f85cf97dd33f78a415249a80
td-usc <89547497+td-usc@users.noreply.github.com>
2022-06-08 11:23:19 -0700

    Added sidebar color change to word
    Added functionality to check word after each letter collected and changer sidebar color.

--- Assets/Scripts/Word.cs
+++ Assets/Scripts/Word.cs
@@ -11,9 +11,14 @@ public class Word : MonoBehaviour
 
     [SerializeField] private List<SpriteRenderer> sprites = new List<SpriteRenderer>();
 
+    [SerializeField] private SpriteRenderer leftSidebar;
+    [SerializeField] private SpriteRenderer rightSidebar;
+
     private List<LetterClass> letters = new List<LetterClass>();
 
     private int currentLetterBox = 0;
+
+    private string word = "";
 
     void Update()
     {
@@ -28,26 +33,33 @@ public class Word : MonoBehaviour
         else
         {
             letters.Add(newLetter);
+            word += newLetter.Letter;
             sprites[currentLetterBox].sprite = newLetter.LetterSprite;
             currentLetterBox++;
+
+            // Update Sidebars
+            bool valid = evaluator.IsValidWord(word);
+            if (valid) {
+                //leftSidebar.color = Color.green;
+                //rightSidebar.color = Color.green;
+            }
+            else {
+                //leftSidebar.color = Color.red;
+                //rightSidebar.color = Color.red;
+            }
         }
 
         return true;
     }
 
     public int submitWord() {
-        // Make list into string
-
-        string word = "";
-
-        foreach (LetterClass l in letters) word += l.Letter;
-
         // Check validity and get word score
         // If valid, clear list
 
         int score = evaluator.SubmitWord(word);
 
         letters.Clear();
+        word = "";
         foreach (SpriteRenderer sr in sprites) sr.sprite = defaultSprite;
         currentLetterBox = 0;
 
