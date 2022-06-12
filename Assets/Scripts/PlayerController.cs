c6bc854daea59952d696772290c377cc90bf3bae
RobbieGoldberg <robbiebryce1998@gmail.com>
2022-06-11 23:39:51 -0700

    reverted tweak

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -145,7 +145,7 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = Vector3.MoveTowards(transform.position, bounceBackTargetPos, Time.deltaTime * bounceBackSpeed);
 
-            bounceBackSpeed *= 0.985f;
+            bounceBackSpeed *= 0.98f;
 
             if (bounceBackSpeed < 1)
             {
