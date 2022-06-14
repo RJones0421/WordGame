81425f3850d5c4db73a53bdd3a4488d7d951b10d
mrw0505 <43623820+mrw0505@users.noreply.github.com>
2022-06-14 03:42:37 -0700

    Fixed Menu Sizing
    Fixed menu sizing and a did a little resizing and button changes. Still working on showing control icons.

--- Assets/Scripts/UI/Button_Music_Switch.cs
+++ Assets/Scripts/UI/Button_Music_Switch.cs
@@ -10,6 +10,26 @@ public class Button_Music_Switch : MonoBehaviour
 	public Sprite tempSprite;
 	public Image original;
 
+	public GameObject controlSprite;
+
+	void Start()
+    {
+
+    	controlSprite.SetActive(false);
+
+    }
+
+	void Update()
+    {
+
+		if (Input.GetKeyDown(KeyCode.M))
+        {
+            controlSprite.SetActive(true);
+            print("space key was pressed");
+        }
+
+    }
+
 	public void ChangeImage() {
 
 		if (original.sprite == tempSprite) {
