4d2738c63864bb56d9b0800667e932630247c679
WenYang-Lu <93624480+WenYang-Lu@users.noreply.github.com>
2022-06-20 16:55:09 -0700

    add a tutorial scene

--- Assets/Scripts/Platform/NewLetterPlatform.cs
+++ Assets/Scripts/Platform/NewLetterPlatform.cs
@@ -8,9 +8,14 @@ public class NewLetterPlatform : Platform
     public Collectible collectible;
     private bool isCollected = false;
 
+    public GameObject letterArrow;
+
     public override void Activate()
     {
         CollectLetter();
+        if (letterArrow){
+            letterArrow.SetActive(false);
+        }
     }
 
     public SpriteRenderer SpriteRenderer {
@@ -60,5 +65,11 @@ public class NewLetterPlatform : Platform
             DarkenSprite();
         }
         */
+    }
+    public void ActivateArrow()
+    {
+        if (letterArrow){
+            letterArrow.SetActive(true);
+        }
     }
 }
