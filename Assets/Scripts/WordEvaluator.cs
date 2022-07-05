97559d44ffea39024b358c4acb0a7702257c64e4
Shreyas Bhat <shreyasbhat95@gmail.com>
2022-07-04 22:53:40 -0700

    Added anagram check during word submission

--- Assets/Scripts/WordEvaluator.cs
+++ Assets/Scripts/WordEvaluator.cs
@@ -41,6 +41,10 @@ public class WordEvaluator : MonoBehaviour
 
 	public bool IsValidWord(string word)
 	{
+		//if anagram powerup is activated check if word matches 
+		if(PlayerPrefs.GetInt(ShopConstants.SHOP_POWERUP_ANAGRAM_ACTIVATED,0)==1){
+			return dictionaries.VerifyWordAnagram(word);
+		}
 		return dictionaries.VerifyWord(word);
 	}
 
