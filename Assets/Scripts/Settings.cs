f22e52abf987d66e707763d9710e52fd2b7b465f
td-usc <89547497+td-usc@users.noreply.github.com>
2022-07-13 15:43:18 -0700

    Fixed settings UI carryover from PlayerPrefs

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
+
+    [SerializeField] Slider controlsSlider;
+    [SerializeField] Slider sensitivitySlider;
+
     void Awake() 
     {
-        PlayerPrefs.SetInt("controls", 0);
-        PlayerPrefs.SetFloat("sensitivity", 1.0f);
+        if(!PlayerPrefs.HasKey("controls")) PlayerPrefs.SetInt("controls", 0);
+        else controlsSlider.value = PlayerPrefs.GetInt("controls");
+        
+        if(!PlayerPrefs.HasKey("sensitivity")) PlayerPrefs.SetFloat("sensitivity", 1.0f);
+        else sensitivitySlider.value = PlayerPrefs.GetFloat("sensitivity");
     }
 
     public void SetControls(float setting) { PlayerPrefs.SetInt("controls", (int)setting); }
