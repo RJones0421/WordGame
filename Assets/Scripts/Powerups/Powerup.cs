4d8764b570b6a2798a314df9491eead8ce2a9dae
Jae Shim <jaeshim@usc.edu>
2022-07-19 05:26:42 -0700

    Replacing powerup

---
+++ Assets/Scripts/Powerups/Powerup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Powerup")]
+public abstract class Powerup : Collectible
+{
+    [SerializeField] private string power;
+
+    public string Power { get { return power; } }
+
+    public abstract bool Activate();
+}
