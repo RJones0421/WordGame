1918eb6f953035c4588b288d604fc0ae8ec7c7f6
Jason Chen <chenjase@umich.edu>
2022-07-20 14:26:28 -0400

    fixed shop UI display bug of not showing balance when first open

---
+++ Assets/Scripts/UI/Shop_UI_Object.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop_UI_Object : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("START OF SHOP UI OBJECT");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/UI/Shop_UI_Object2.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class Shop_UI_Object2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("START OF SHOP_PURCHASE");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    public void displayCurrency_onAppear(){
+        GameObject inputFieldGo = GameObject.Find("Text_Shop_Currency");
+        TMP_Text inputFieldCo = inputFieldGo.GetComponent<TMP_Text>();
+        int pointsAvailable = PlayerPrefs.GetInt(CurrencyUtils.currency_amount_keyname);
+        Debug.Log("curreny amount display in shop opening: " + pointsAvailable);
+        inputFieldCo.text = "Points Available: " + pointsAvailable;
+
+        CurrencyUtils.displayQuantity("1");
+        CurrencyUtils.displayQuantity("2");
+        CurrencyUtils.displayQuantity("3");
+        CurrencyUtils.displayQuantity("4");
+    }
+
+
+
+}
