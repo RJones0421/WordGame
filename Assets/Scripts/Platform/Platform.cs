ad8a00b3d78221758aa8dd9b78d1d7b00acecf88
Jae Shim <jaeshim@usc.edu>
2022-07-19 19:23:51 -0700

    Fixed animation build bugs

--- Assets/Scripts/Platform/Platform.cs
+++ Assets/Scripts/Platform/Platform.cs
@@ -19,25 +19,27 @@ public class Platform : MonoBehaviour
         chalkParticles = GetComponent<ParticleSystem>();
 
         // Initialize and position animation
-        animate = new GameObject("Animate").transform;
+        animate = Instantiate(GameObject.Find("Player").transform.GetChild(0)).transform;
         animate.parent = transform;
         animate.SetPositionAndRotation(transform.position, transform.rotation);
         animate.localScale = Vector3.one;
 
         // Setup animator
-        Animator animator = animate.gameObject.AddComponent<Animator>();
-        animator.runtimeAnimatorController = AssetDatabase.LoadAssetAtPath("Assets/Animations/Height.controller", typeof(RuntimeAnimatorController)) as RuntimeAnimatorController;
-        animator.keepAnimatorControllerStateOnDisable = true;
+        //Animator animator = animate.gameObject.AddComponent<Animator>();
+        //animator.runtimeAnimatorController = Instantiate(GameObject.Find("Player").transform.GetChild(0));
+        //animator.keepAnimatorControllerStateOnDisable = true;
 
         // Initialize and position sprite
-        Transform coke = new GameObject("Sprite").transform;
-        coke.parent = animate;
-        coke.SetPositionAndRotation(animate.position, animate.rotation);
-        coke.localScale = Vector3.one;
+        //Transform coke = new GameObject("Sprite").transform;
+        //coke.parent = animate;
+        //coke.SetPositionAndRotation(animate.position, animate.rotation);
+        //coke.localScale = Vector3.one;
 
-        // Set new sprite and disable old sprite
-        spriteRenderer = coke.gameObject.AddComponent<SpriteRenderer>();
-        spriteRenderer.sprite = sprite ? sprite : AssetDatabase.LoadAssetAtPath("Assets/Sprites/Handwritten_UnFilled/Letter_EMPTY.png", typeof(Sprite)) as Sprite;
+        //// Set new sprite and disable old sprite
+        spriteRenderer = animate.GetChild(0).GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprite ? sprite : GameObject.Find("GameManager").GetComponent<GlobalVariables>().blank;
+        //spriteRenderer = coke.gameObject.AddComponent<SpriteRenderer>();
+        //spriteRenderer.sprite = GameObject.Find("GameManager").GetComponent<GlobalVariables>().blank;
     }
 
     public SpriteRenderer SpriteRenderer
